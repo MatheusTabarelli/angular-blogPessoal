b2edb970fd8aa4bde1fc7868a0929435634d8919
Matheus Tabarelli <12tabarelli@gmail.com>
2021-08-30 22:38:15 -0300

    Implementando Entrar e Cadastrar com o Banco de Dados

---
+++ src/app/model/UsuarioLogin.ts
@@ -0,0 +1,8 @@
+export class UsuarioLogin {
+    public id: number
+    public nome: string
+    public senha: string
+    public usuario: string
+    public token: string 
+    public foto: string
+}
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
+    return this.http.post<UsuarioLogin>('https://matheustabarelli.herokuapp.com/usuarios/logar',
+      usuarioLogin)
+  }
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>('https://matheustabarelli.herokuapp.com/usuarios/cadastrar',
+      usuario)
+  }
+}
